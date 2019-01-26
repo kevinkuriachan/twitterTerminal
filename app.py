a7938d3ba7297a8a042cc83e920819b948d899e4
Kevin Kuriachan <kuriachankevin@gmail.com>
2019-01-26 17:46:46 -0600

    testing

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 GM_BOT_ID = os.getenv('bot_id')
 twitterAPI = getAPI()
 
-twitterAPI.update_status("app running")
+twitterAPI.update_status("app running") 
 
 
 @app.route('/')
