f6da9c14b19b9d2b4fe2e1f2e0deab4c2bcc5204
Kevin Kuriachan <kuriachankevin@gmail.com>
2019-01-26 17:59:55 -0600

    created post method

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 twitterAPI = getAPI()
 
 
-
 @app.route('/')
+def home():
+    return "hello"
+
+@app.route('/recieve', methods=['POST'])
 def recieveMessage():
     data = request.get_json()
     print("**MESSAGE RECIEVED**")
 
     twitterAPI.update_status(data['text'])
 
-    return("hello")
+    return "ok", 200
