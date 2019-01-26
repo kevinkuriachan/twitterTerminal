b330bcc8b99879fe0ab9c93ff0b7aebe89668e3b
Kevin Kuriachan <kuriachankevin@gmail.com>
2019-01-26 17:46:20 -0600

    testing

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 GM_BOT_ID = os.getenv('bot_id')
 twitterAPI = getAPI()
 
+twitterAPI.update_status("app running")
+
 
 @app.route('/')
 def recieveMessage():
     data = request.get_json()
+    print("**MESSAGE RECIEVED**")
+
     twitterAPI.update_status(data['text'])
 
