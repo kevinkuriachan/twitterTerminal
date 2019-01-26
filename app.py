cacb999ff74d9ab151b6ffae34fd5efe61dc759d
Kevin Kuriachan <kuriachankevin@gmail.com>
2019-01-26 17:54:01 -0600

    added another requirement

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 import sys, json, os, urllib, random
 from config.twitter_client import getAPI
 
 app = Flask(__name__)
 
-GM_BOT_ID = os.getenv('bot_id')
+#GM_BOT_ID = os.getenv('bot_id')
 twitterAPI = getAPI()
 
-twitterAPI.update_status("app running") 
+#twitterAPI.update_status("app running") 
 
 
 @app.route('/')
@@ -17,3 +17,4 @@ def recieveMessage():
 
     twitterAPI.update_status(data['text'])
 
+    return("hello")
