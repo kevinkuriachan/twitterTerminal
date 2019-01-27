5176a8ed0b660c41777c8ce027358699296390b6
Kevin Kuriachan <kuriachankevin@gmail.com>
2019-01-26 18:59:30 -0600

    added tweet pic from any sub ability

--- app.py
+++ app.py
@@ -2,11 +2,36 @@ from flask import Flask, request
 import sys, json, os, urllib, random
 from config.twitter_client import getAPI
 import praw, random, requests
+from prawcore import NotFound as subNotFound
+from urllib.request import Request, urlopen
 
 app = Flask(__name__)
 
+BOT_ID = os.getenv('bot_id')
+
+
+
 twitterAPI = getAPI()
 redditAPI = reddit = praw.Reddit(client_id = os.getenv('redditID'), client_secret = os.getenv('redditSecret'), user_agent = 'my user agent')
+
+def subreddit_exists(name):
+    # reference: https://www.reddit.com/r/redditdev/comments/68dhpm/praw_best_way_to_check_if_subreddit_exists_from/
+    exists = True
+    try:
+        redditAPI.subreddits.search_by_name(sub, exact=True)
+    except subNotFound:
+        exists = False
+    return exists
+
+def respond(message):
+    url = "https://api.groupme.com/v3/bots/post"
+    data = {
+				'bot_id' : BOT_ID,
+				'text' : message,
+			}
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
+
 
 @app.route('/')
 def home():
@@ -21,16 +46,19 @@ def recieveMessage():
         twitterAPI.update_status(stringToTweet)
         return "ok", 200
 
-    if ("@bot#pic: cat" in data['text']):
+    if ("@bot#pic: " in data['text']):
         sub = data['text'].replace("@bot#pic: ", "")
-        print("SUBREDDIT: " + sub)
-        submissions = list(redditAPI.subreddit('cat').hot(limit=150))
-        size = len(submissions)
-        url = submissions[random.randint(0,size)].url
-        req = requests.get(url)
-        with open('imgToTweet.jpg', 'wb') as openFile:
-            openFile.write(req.content)
-        twitterAPI.update_with_media("imgToTweet.jpg")
+        if (subreddit_exists(sub)):
+            submissions = list(redditAPI.subreddit(sub).hot(limit=150))
+            size = len(submissions)
+            url = submissions[random.randint(0,size)].url
+            req = requests.get(url)
+            with open('imgToTweet.jpg', 'wb') as openFile:
+                openFile.write(req.content)
+            twitterAPI.update_with_media("imgToTweet.jpg")
+        else:
+            respond("subreddit not found for " + sub)
+
         return "ok", 200
 
     return "ok", 200
